57cd1141facfbfff28c288a44892aedf3b10372b
ManuaID <matthieu.kwong@gmail.com>
2025-08-26 14:40:05 +1000

    Finished implementation for refraction however some offset to given example image

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 
         shared_ptr<material> ground_material = make_shared<labertian>(color(0.8,0.8,0.0));
         shared_ptr<material> mat_centre = make_shared<labertian>(color(0.1,0.2,0.5));
-        shared_ptr<material> mat_left = make_shared<metal>(color(0.8,0.8,0.8),0.3);
+        shared_ptr<material> mat_left   = make_shared<dielectric>(1.50);
         shared_ptr<material> mat_right = make_shared<metal>(color(0.8,0.6,0.2),1.0);
 
         world.add(make_shared<sphere>(point3(0,0,-1), 0.5,mat_centre));
@@ -22,8 +22,8 @@
 
         camera cam;
         cam.aspect_ratio = 16.0/9.0;
-        cam.image_width = 1080;
-        cam.pixel_sample_size = 100;
+        cam.image_width = 400;
+        cam.pixel_sample_size = 100 ;
         cam.max_depth = 50;
 
         cam.render(world);
--- src/vec.h
+++ src/vec.h
@@ -1,6 +1,8 @@
 #ifndef VEC_H
 #define VEC_H
 
+#include <cmath>
+
 class vec3{
     public:
         /*
@@ -160,6 +162,14 @@ inline vec3 random_on_hemisphere(const vec3& normal) {
 
 inline vec3 reflect_value(const vec3& u, const vec3& normal) {
     return u - 2*(dot(u,normal)) * normal;
+}
+
+inline vec3 refract(const vec3& uv,const vec3& normal,double refract_index_divised) {
+    double cos_theta = std::fmin(dot(-uv,normal), 1.0);
+    vec3 r_perp = refract_index_divised*(uv + cos_theta*normal);
+    vec3 r_para = -std::sqrt(std::fabs(1.0 - r_perp.length_squared())) * normal;
+
+    return r_perp + r_para;
 }
 
 #endif
